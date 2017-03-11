994bc6617203b704911e98aa6eb822d0a16ddc63
MQuy <sugiacupit@gmail.com>
2017-03-11 16:56:01 +0800

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -4,30 +4,25 @@ defmodule PhoenixMjml.Mixfile do
   def project do
     [app: :phoenix_mjml,
      version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+     elixir: "~> 1.2",
+     deps: deps(),
+     package: [
+       maintainers: ["MQuy"],
+       licenses: ["MIT"],
+       links: %{github: "https://github.com/MQuy/phoenix_mjml"}
+     ],
+     description: """
+     Phoenix Template Engine for Mjml
+     """]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [applications: [:phoenix, :uuid]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:phoenix, "~> 1.2"},
+     {:phoenix_html, "~> 2.3"},
+     {:uuid, "~> 1.1"}]
   end
 end
