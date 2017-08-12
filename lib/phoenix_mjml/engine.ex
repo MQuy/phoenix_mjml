89c1bafcd17c9dd402229b1781e4b42355968bb2
julien-leclercq <julien.leclercq@derniercri.io>
2017-08-12 13:12:16 +0200

    Update MJML strategy with stdout output

--- lib/phoenix_mjml/engine.ex
+++ lib/phoenix_mjml/engine.ex
@@ -5,35 +5,10 @@ defmodule PhoenixMjml.Engine do
 
   def compile(path, _name) do
     path
-    |> generate_html_path
-    |> compile_content
+    case System.cmd("mjml", [path, "-s"]) do
+      {html, 0} -> EEx.compile_string(html, engine: Phoenix.HTML.Engine, line: 0)
+      _         -> raise "Error MJML exited with non zero status"
+    end
   end
 
-  def generate_html_path(path) do
-    path
-    |> generate_mjml_path
-    |> Path.absname
-    ~> convert_into_html(path)
-  end
-
-  def generate_mjml_path(path) do
-    filename =
-      path
-      |> Path.basename
-      |> Path.rootname
-    uuid = UUID.uuid1()
-    "tmp/#{uuid}-#{filename}"
-  end
-
-  def convert_into_html(html_path, mjml_path) do
-    File.mkdir("tmp")
-    System.cmd("mjml", [mjml_path, "-o", html_path])
-  end
-
-  def compile_content(html_path) do
-    html_path
-    |> File.read!
-    >>> File.rm!(html_path)
-    |> EEx.compile_string(engine: Phoenix.HTML.Engine, line: 0, trim: true)
-  end
 end
