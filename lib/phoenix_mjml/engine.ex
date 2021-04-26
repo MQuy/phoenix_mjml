7e18839fafb02c0ada44c1c6ade12d84ac01c842
technicalcapt <technicalcapt@gmail.com>
2021-04-26 12:14:15 +0700

    add compile support for windows

--- lib/phoenix_mjml/engine.ex
+++ lib/phoenix_mjml/engine.ex
@@ -2,10 +2,19 @@ defmodule PhoenixMjml.Engine do
   @behaviour Phoenix.Template.Engine
 
   def compile(path, _name) do
-    case System.cmd("mjml", [path, "-s"]) do
+    os_type = :os.type()
+
+    case do_compile_with(os_type) do
       {html, 0} -> EEx.compile_string(html, engine: Phoenix.HTML.Engine, line: 0)
-      _         -> raise "Error MJML exited with non zero status"
+      _ -> raise "Error MJML exited with non zero status"
     end
   end
 
+  defp do_compile_with({:unix, _osname}) do
+    System.cmd("mjml", [path, "-s"])
+  end
+
+  defp do_compile_with({:win32, _osname}) do
+    System.cmd("cmd.exe", ["/c", "mjml", path, "-s"])
+  end
 end
